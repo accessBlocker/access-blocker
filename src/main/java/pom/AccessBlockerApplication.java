5b9dffa5d39eed933f9cb7adcf74c10ab78da72b
Georgy Levin <g.phyzy@gmail.com>
2024-06-14 20:33:10 +0300

    initial commit

---
+++ src/main/java/pom/AccessBlockerApplication.java
@@ -0,0 +1,13 @@
+package pom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessBlockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccessBlockerApplication.class, args);
+	}
+
+}
